2f5b93af427ec00f37c9953bb9caf85698dcd82d
z004310x <18795607764@163.com>
2024-09-19 08:51:10 +0800

    HelloWorld

--- HelloWorld.py
+++ HelloWorld.py
@@ -7,8 +7,8 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print('Hello World!!')
-
+    print('Hello World!')
+    print('回首向来萧瑟处，归去，也无风雨也无晴。')
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
